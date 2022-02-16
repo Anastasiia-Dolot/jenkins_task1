ba0dd5a4250afa787589bd8e767156fc421b2a06
Anastasiia Dolot <83067897+Anastasiia-Dolot@users.noreply.github.com>
2022-02-16 09:00:47 +0200

    Update db.php

--- php-mysql-crud/db.php
+++ php-mysql-crud/db.php
@@ -2,13 +2,9 @@
 session_start();
 
 $conn = mysqli_connect(
-  'localhost',
+  'anastasiia-dolot.microsoft.com',
   'admin1',
-<<<<<<< HEAD
   'Tg45Sc!d1',
-=======
-  'password123',
->>>>>>> bcc2100b41b07c185a91fa82cdf972e0f210cc40
   'php_mysql_crud'
 ) or die(mysqli_error($mysqli));
 
